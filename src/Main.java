39220fecb1aad1194838a34769989c49f4d082ef
gofuuse <nihad.mammadov.16@gmail.com>
2025-02-22 14:09:10 +0400

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<String> newList = List.of("apple","banana","apple","orange","banana","banana");
+        int k = 2;
+        List<String> resultList = newList.stream()
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Comparator.<Map.Entry<String, Long>>comparingLong(Map.Entry::getValue)
+                        .reversed()
+                        .thenComparing(Map.Entry::getKey))
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .toList();
+        System.out.println(resultList);
     }
 }
